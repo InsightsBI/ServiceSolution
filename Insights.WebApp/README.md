8d04fe3b3097f036a4aa9e3b7c819ca44d4f7f05
Swaraj Mahajan <iamswarajmahajan@gmail.com>
2017-10-03 23:54:38 +0530

    Add project files.

---
+++ Insights.WebApp/README.md
@@ -0,0 +1,3 @@
+﻿# Insights.WebApp
+
+
---
+++ Insights/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Insights.DataLayer.Tables;
+using Newtonsoft.Json.Linq;
+using Insights.Models;
+
+namespace Insights.Controller
+{
+    public class UserController : ApiController
+    {
+        [Route("api/usercontroller/{data}")]
+        [HttpPost]
+       public void  StoreUserDetails(UserModel data)
+        {
+            //User tableData = new DataLayer.Tables.User
+            //{
+            //    UserId = data.UserId,
+            //    FirstName = data.FirstName,
+            //    LastName = data.LastName,
+            //    UserName = data.UserName,
+            //    Password = data.Password,
+            //    CreatedDate = data.CreatedDate,
+            //    UpdatedDate = data.UpdatedDate,
+            //    IsActive = data.IsActive
+            //};
+            using (var tableEntity = new insightbiModelEntities())
+            {
+               
+
+                tableEntity.Users.Add(tableData);
+                tableEntity.SaveChanges();
+
+            }
+          
+        }
+        [Route("api/usercontroller/{id}")]
+        [HttpGet]
+        public User GetUserDetails(int id)
+        {
+            
+            insightbiModelEntities tableEntity = new insightbiModelEntities();
+            tableEntity.Users.Where(t => t.UserId == id);
+            User u = new User();
+            return u;
+        }
+    }
+}
